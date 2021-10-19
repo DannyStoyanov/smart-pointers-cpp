efb0cb330be07efa7f0b9e1bababc16c8259fb43
Danny <36959138+DannyStoyanov@users.noreply.github.com>
2021-10-19 13:35:20 +0300

    add unique_ptr operators

--- unique_ptr.h
+++ unique_ptr.h
@@ -2,11 +2,12 @@
 #include <iostream>
 #include <memory>
 
-template<typename T, typename Deleter = std::default_delete<T>>
+template<class T, class Deleter = std::default_delete<T>>
 class unique_ptr {
 private:
 	T* ptr;
 	Deleter del;
+
 public:
 	// Constructors:
 	constexpr unique_ptr() noexcept;
@@ -14,7 +15,7 @@ public:
 	explicit unique_ptr(T* ptr) noexcept;
 	unique_ptr(T* ptr, Deleter del) noexcept;
 
-	// std::unique_ptr<T,Deleter>::operator=
+	// std::unique_ptr<T, Deleter>::operator=
 	unique_ptr<T, Deleter>& operator= (std::nullptr_t) noexcept;
 	unique_ptr<T, Deleter>& operator=(const unique_ptr<T, Deleter>&& ptr) noexcept;
 
@@ -38,40 +39,39 @@ public:
 	// Operators:
 	T* operator->() const noexcept;
 	T operator*() const noexcept;
-	
 };
 
 // Constructors:
-template <typename T, typename Deleter>
+template <class T, class Deleter>
 constexpr unique_ptr<T, Deleter>::unique_ptr() noexcept {
 	this->ptr = nullptr;
 	std::default_delete<T> obj;
 	this->del = obj; //std::default_delete<T>::default_delete()
 }
 
-template <typename T, typename Deleter>
+template <class T, class Deleter>
 constexpr unique_ptr<T, Deleter>::unique_ptr(std::nullptr_t) noexcept: unique_ptr() {};
 
-template <typename T, typename Deleter>
+template <class T, class Deleter>
 unique_ptr<T, Deleter>::unique_ptr(T* ptr) noexcept {
 	this->ptr = ptr;
 	std::default_delete<T> obj;
 	this->del = obj; //std::default_delete<T>::default_delete()
 }
 
-template <typename T, typename Deleter>
+template <class T, class Deleter>
 unique_ptr<T, Deleter>::unique_ptr(T* ptr, Deleter del) noexcept {
 	this->ptr = ptr;
 	this->del = del;
 }
 
-// std::unique_ptr<T,Deleter>::operator=
-template <typename T, typename Deleter>
-unique_ptr<T, Deleter>& unique_ptr<T, Deleter>::operator= (std::nullptr_t) noexcept {
+// std::unique_ptr<T, Deleter>::operator=
+template <class T, class Deleter>
+unique_ptr<T, Deleter>& unique_ptr<T, Deleter>::operator=(std::nullptr_t) noexcept {
 	reset();
 }
 
-template <typename T, typename Deleter>
+template <class T, class Deleter>
 unique_ptr<T, Deleter>& unique_ptr<T, Deleter>::operator=(const unique_ptr<T, Deleter>&& ptr) noexcept {
 	if(*this != &ptr) {
 		this->swap(ptr);
@@ -80,22 +80,22 @@ unique_ptr<T, Deleter>& unique_ptr<T, Deleter>::operator=(const unique_ptr<T, De
 }
 
 // Destructor:
-template <typename T, typename Deleter>
+template <class T, class Deleter>
 unique_ptr<T, Deleter>::~unique_ptr() {
 	if(!(get() == nullptr)) {
 		get_deleter()(get());
 	}
-
 }
+
 // Modifiers:
-template <typename T, typename Deleter>
+template <class T, class Deleter>
 T* unique_ptr<T, Deleter>::release() noexcept {
 	T* temp = get();
 	this->ptr = nullptr;
 	return temp;
 }
 
-template <typename T, typename Deleter>
+template <class T, class Deleter>
 void unique_ptr<T, Deleter>::reset(T* ptr) noexcept {
 	T* old_ptr = this->ptr;
 	this->ptr = ptr;
@@ -104,36 +104,74 @@ void unique_ptr<T, Deleter>::reset(T* ptr) noexcept {
 	}
 }
 
-template <typename T, typename Deleter>
+template <class T, class Deleter>
 void unique_ptr<T, Deleter>::swap(unique_ptr<T, Deleter>& other) noexcept {
 	std::swap(this->ptr, other.ptr);
 	std::swap(this->del, other.del);
 }
 
 // Observers:
-template <typename T, typename Deleter>
+template <class T, class Deleter>
 T* unique_ptr<T, Deleter>::get() const noexcept {
 	return this->ptr;
 }
 
-template <typename T, typename Deleter>
+template <class T, class Deleter>
 Deleter& unique_ptr<T, Deleter>::get_deleter() noexcept {
 	return this->del;
 }
 
-template <typename T, typename Deleter>
+template <class T, class Deleter>
 unique_ptr<T, Deleter>::operator bool() const noexcept {
 	return get() != nullptr;
 }
 
-
 // Operators:
-template <typename T, typename Deleter>
+
+// Member operators:
+template <class T, class Deleter>
 T* unique_ptr<T, Deleter>::operator->() const noexcept {
 	return get();
 }
 
-template <typename T, typename Deleter>
+template <class T, class Deleter>
 T unique_ptr<T, Deleter>::operator*() const noexcept {
 	return *get();
+}
+
+// Non-member operators:
+template <class T1, class Deleter1, class T2, class Deleter2>
+bool operator==(const unique_ptr<T1, Deleter1>& x, const unique_ptr<T2, Deleter2>& y) {
+	return x.get() == y.get();
+}
+
+template <class T, class Deleter>
+bool operator==(const unique_ptr<T, Deleter>& x, std::nullptr_t) noexcept {
+	return !x;
+}
+
+template <class T, class Deleter>
+bool operator==(std::nullptr_t, const unique_ptr<T, Deleter>& x) noexcept {
+	return !x;
+}
+
+template <class T1, class Deleter1, class T2, class Deleter2>
+bool operator!=(const unique_ptr<T1, Deleter1>& x, const unique_ptr<T2, Deleter2>& y) {
+	return x.get != y.get();
+}
+
+template <class T, class Deleter>
+bool operator!=(const unique_ptr<T, Deleter>& x, std::nullptr_t) noexcept {
+	return (bool)x;
+}
+
+template <class T, class Deleter>
+bool operator!=(std::nullptr_t, const unique_ptr<T, Deleter>& x) noexcept {
+	return (bool)x;
+}
+
+// make_unique<T>
+template< class T, class... Args >
+unique_ptr<T> make_unique( Args&&... args ) {
+	return unique_ptr<T>(new T(std::forward<Args>(args)...));
 }
